e1b733dcee31f572eaf2ba726738f26677ae1f74
Guilherme Lima <guilhermelima@Guilhermes-Mac-Studio.local>
2022-10-17 09:35:10 -0300

    setup

--- figma/admin.py
+++ figma/admin.py
@@ -1,4 +1,15 @@
 from django.contrib import admin
 from figma.models import Figma
+from django.utils.html import format_html
 
-admin.site.register(Figma)
+@admin.register(Figma)
+class FigmaAdmin(admin.ModelAdmin):
+    list_display = ('id','nome', 'descricao', 'link_figma_url', 'mobile', 'status',)
+    list_display_links = ('id', 'nome', 'link_figma_url',)
+    search_fields = ('nome',)
+    list_filter = ('status', )
+    list_editable = ('status',)
+    list_per_page = 25
+
+    def link_figma_url(self, obj):
+        return format_html("<a href='{url}' target='_blank'>{url}</a>", url=obj.link_figma)
