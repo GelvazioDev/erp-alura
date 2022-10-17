7265e0444c3896f0bf62a9d19248b4cf5c7974ba
Guilherme Lima <guilhermelima@Guilhermes-Mac-Studio.local>
2022-10-17 10:05:26 -0300

    setup

--- setup_producao/admin.py
+++ setup_producao/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from setup_producao.models import Setup
 
-admin.site.register(Setup)
+@admin.register(Setup)
+class SetupAdmin(admin.ModelAdmin):
+    list_display = ('id','instrutor',)
+    list_display_links = ('id', 'instrutor',)
+    search_fields = ('instrutor',)
+    list_per_page = 25
