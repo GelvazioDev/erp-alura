3a88238c9505659d8d60d23ec368d5460e782ba4
Guilherme Lima <guilhermelima@Guilhermes-Mac-Studio.local>
2022-10-17 14:52:35 -0300

    endereco

--- contatos/models.py
+++ contatos/models.py
@@ -7,7 +7,7 @@ class Contato(models.Model):
     email = models.CharField(max_length=100)
     cargo = models.CharField(max_length=20, choices=TIPOS_DE_CARGOS, blank=False, null=False,default='0')
     telefone = models.CharField(max_length=15, blank=True, null=True)
-    endereco = models.CharField(max_length=100, blank=True, null=True)
+    endereco = models.TextField(max_length=300, blank=True, null=True)
     carga_horaria = models.CharField(max_length=20, choices=CARGA, blank=False, null=False,default='0')
 
 
